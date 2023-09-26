163825a2a8f5cccc0ea891feae998fa2e640eb29
Monica Björk <m.bjork95@gmail.com>
2023-09-26 10:48:29 +0200

    typos

--- game.js
+++ game.js
@@ -1,31 +1,33 @@
 const { Client, Account, Databases, ID, Query } = Appwrite;
-const projectID = '6511e7ed14ed7455d2e1'; // TODO make secret
+const projectId = '6511e7ed14ed7455d2e1'; // TODO make secret
 const databaseId = '';
 const collectionId = '';
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(projectId)
+.setEndpoint('https://cloud.appwrite.io/v1')
+.setProject(projectId)
 ;
 
 const account = new Account(client);
 
+
 function register(event) {
+    event.preventDefault();
     console.log("event", event);
     // TODO check incoming values from elements
-    Account.create(
-        ID.unique,
+    account.create(
+        ID.unique(),
         event.target.elemtents['register-email'].value,
         event.target.elemtents['register-password'].value,
         event.target.elemtents['register-username'].value
     ), then(response => {
         console.log("Account create response",response);
         // TODO create a document in a database
+
         account.createEmailSession(
             event.target.elemtents['register-email'].value,
             event.target.elemtents['register-password'].value
         );
-    }).catch(error => console.error(error));
+    }).catch(error => console.error("Account create error", error));
 
-    event.preventDefault();
 }
