2b16fdee0b2c3646e4d5b8c0bed884cab95e0c41
Monica Björk <m.bjork95@gmail.com>
2023-10-11 14:26:26 +0200

    All tilesets added

--- public/game.js
+++ public/game.js
@@ -244,47 +244,114 @@ function startGame() {
             jump: 21
         },
     });
-    loadSprite("grass", "sprites/nature-platformer-tileset-16x16.png", {
+    loadSprite("tiles", "sprites/nature-platformer-tileset-16x16.png", {
         sliceX: 7,
         sliceY: 11,
-        // area: {vec2: [16, 0], vec2: [32, 16]}
-        // area: {
-        //     x: 16,
-        //     y: 0,
-        //     width: 16,
-        //     height: 16,
-        // }
     });
-    
 
     scene("game", ({ level, score }) => {
         layers(["bg", "obj", "ui"], "obj");
         // gravity(0);
 
         const levelConfig = {
-            width: 16,
-            height: 16,
-            '(' : [ sprite('grass'),                   solid(), {frame:0} ],
-            '=' : [ sprite('grass'),                   solid(), {frame:1} ],
-            ')' : [ sprite('grass'),                   solid(), {frame:2} ],
+            width: 15,
+            height: 15,
+            // Dirt top
+            '(' : [ sprite('tiles'), solid(), {frame:0} ],
+            '0' : [ sprite('tiles'), solid(), {frame:1} ],
+            ')' : [ sprite('tiles'), solid(), {frame:2} ],
+            // Dirt middle
+            '[' : [ sprite('tiles'), solid(), {frame:7} ],
+            '1' : [ sprite('tiles'), solid(), {frame:8} ],
+            ']' : [ sprite('tiles'), solid(), {frame:9} ],
+            // Dirt bottom
+            '<' : [ sprite('tiles'), solid(), {frame:14} ],
+            '2' : [ sprite('tiles'), solid(), {frame:15} ],
+            '>' : [ sprite('tiles'), solid(), {frame:16} ],
+            // Dirt floor
+            'A' : [ sprite('tiles'), solid(), {frame:3} ],
+            '3' : [ sprite('tiles'), solid(), {frame:4} ],
+            'a' : [ sprite('tiles'), solid(), {frame:5} ],
+            // Brick wall
+            'B' : [ sprite('tiles'), solid(), {frame:6} ],
+            '4' : [ sprite('tiles'), solid(), {frame:13} ],
+            'b' : [ sprite('tiles'), solid(), {frame:20} ],
+            // Brick floor
+            'C' : [ sprite('tiles'), solid(), {frame:42} ],
+            '5' : [ sprite('tiles'), solid(), {frame:43} ],
+            'c' : [ sprite('tiles'), solid(), {frame:44} ],
+            // Brick top
+            ',' : [ sprite('tiles'), solid(), {frame:17} ],
+            '6' : [ sprite('tiles'), solid(), {frame:18} ],
+            '.' : [ sprite('tiles'), solid(), {frame:19} ],
+            // Brick middle
+            ';' : [ sprite('tiles'), solid(), {frame:24} ],
+            '7' : [ sprite('tiles'), solid(), {frame:25} ],
+            ':' : [ sprite('tiles'), solid(), {frame:26} ],
+            // Brick bottom
+            '¤' : [ sprite('tiles'), solid(), {frame:31} ],
+            '8' : [ sprite('tiles'), solid(), {frame:32} ],
+            '#' : [ sprite('tiles'), solid(), {frame:33} ],
+            // Brick smooth
+            'H' : [ sprite('tiles'), solid(), {frame:34} ],
+            'h' : [ sprite('tiles'), solid(), {frame:41} ],
+            // Ladder
+            'D' : [ sprite('tiles'), solid(), {frame:21} ],
+            '9' : [ sprite('tiles'), solid(), {frame:28} ],
+            'd' : [ sprite('tiles'), solid(), {frame:35} ],
+            // Tall plant
+            'E' : [ sprite('tiles'), solid(), {frame:22} ],
+            '!' : [ sprite('tiles'), solid(), {frame:29} ],
+            'e' : [ sprite('tiles'), solid(), {frame:36} ],
+            // Tree top
+            'F' : [ sprite('tiles'), solid(), {frame:23} ],
+            '?' : [ sprite('tiles'), solid(), {frame:30} ],
+            // Tree bottom
+            'f' : [ sprite('tiles'), solid(), {frame:37} ],
+            'g' : [ sprite('tiles'), solid(), {frame:38} ],
+            'G' : [ sprite('tiles'), solid(), {frame:39} ],
+            // Coins
+            'I' : [ sprite('tiles'), {frame:54}, 'coin' ],
+            'i' : [ sprite('tiles'), {frame:55}, 'coin' ],
+            'J' : [ sprite('tiles'), {frame:61}, 'coin' ],
+            'j' : [ sprite('tiles'), {frame:62}, 'coin' ],
+            // Potions
+            'M' : [ sprite('tiles'), {frame:56} ],
+            'm' : [ sprite('tiles'), {frame:57} ],
+            'N' : [ sprite('tiles'), {frame:63} ],
+            'n' : [ sprite('tiles'), {frame:64} ],
+            // Clouds big corner
+            'P' : [ sprite('tiles'),  {frame:51} ],
+            'p' : [ sprite('tiles'),  {frame:52} ],
+            'Q' : [ sprite('tiles'),  {frame:58} ],
+            'q' : [ sprite('tiles'),  {frame:59} ],
+            // Clouds small corner
+            'R' : [ sprite('tiles'),  {frame:65} ],
+            'r' : [ sprite('tiles'),  {frame:66} ],
+            'S' : [ sprite('tiles'),  {frame:72} ],
+            's' : [ sprite('tiles'),  {frame:73} ],
+            // Clouds double corner
+            'T' : [ sprite('tiles'),  {frame:53} ],
+            't' : [ sprite('tiles'), {frame:67} ],
+            // Clouds solid color
+            'U' : [ sprite('tiles'), {frame:60} ],
+            'u' : [ sprite('tiles'), {frame:74} ],
+            // Plants
+            'K' : [ sprite('tiles'), {frame:45} ],
+            'k' : [ sprite('tiles'), {frame:48} ],
+            // Stones
+            'L' : [ sprite('tiles'),  {frame:46} ],
+            'l' : [ sprite('tiles'),  {frame:47} ],
+            // Trapdoor or wooden crate or closed window or whatever
+            'O' : [ sprite('tiles'), solid(), {frame:49} ],
+            'o' : [ sprite('tiles'), solid(), {frame:50} ], 
 
-            '{' : [ sprite('grass'),                   solid(), {frame:3} ],
-            '~' : [ sprite('grass'),                   solid(), {frame:4} ],
-            '}' : [ sprite('grass'),                   solid(), {frame:5} ],
+            'v' : [ sprite('tiles'), solid(), {frame:10} ], // Single dirt block
+            'w' : [ sprite('tiles'), solid(), {frame:27} ], // single brick block
+            'x' : [ sprite('tiles'),  {frame:11}, 'portal' ], // Dirt cave
+            'y' : [ sprite('tiles'),  {frame:40}, 'water' ], // Water
 
-            '[' : [ sprite('grass'),                   solid(), {frame:7} ],
-            '-' : [ sprite('grass'),                   solid(), {frame:8} ],
-            ']' : [ sprite('grass'),                   solid(), {frame:9} ],
-            // '$' : [ sprite('coin'),                                     'coin' ],
-            // '%' : [ sprite('surprise'),             solid(),            'coin-surprise' ],
-            // '*' : [ sprite('surprise'),             solid(),            'mushroom-surprise' ],
-            // '}' : [ sprite('unboxed'),              solid() ],
-            // '(' : [ sprite('pipe-bottom-left'),     solid(), scale(0.5) ],
-            // ')' : [ sprite('pipe-bottom-right'),    solid(), scale(0.5) ],
-            // '-' : [ sprite('pipe-top-left'),        solid(), scale(0.5), 'pipe' ],
-            // '+' : [ sprite('pipe-top-right'),       solid(), scale(0.5), 'pipe' ],
-            // '^' : [ sprite('evil-shroom'),                               'dangerous', body() ],
-            // '#' : [ sprite('mushroom'),             solid(),             'mushroom', body() ],
+            // '@' : [ sprite('name'), solid(), scale(1), body(),'tag','tags', {frame:0} ]
         }
         
         const gameLevel = addLevel(maps[level], levelConfig);
@@ -305,7 +372,7 @@ function startGame() {
 
         const dino = add([
             sprite("doux", solid()),
-            pos(33, height()/3),
+            pos(25, height()/4),
             body(),
             area(vec2(-7, -4), vec2(7, -16)),
             // big(),
@@ -349,14 +416,14 @@ function startGame() {
         //     }
         // }
 
-        // dino.collides('pipe', () => {
-        //     keyPress('s', () => {
-        //         go('game', {
-        //             level: (level + 1) % maps.length,
-        //             score: scoreLabel.value
-        //         })
-        //     })
-        // })
+        dino.collides('portal', () => {
+            keyPress('s', () => {
+                go('game', {
+                    level: (level + 1) % maps.length,
+                    score: scoreLabel.value
+                })
+            })
+        });
         // dino.collides('dangerous', (danger) => {
         //     if(isJumping) {
         //         destroy(danger);
@@ -364,11 +431,18 @@ function startGame() {
         //         go('lose', {score: scoreLabel.value});
         //     }
         // });
-        // dino.collides('coin', (coin) => {
-        //     destroy(coin);
-        //     scoreLabel.value++;
-        //     scoreLabel.text = scoreLabel.value;
-        // });
+        dino.collides('coin', (coin) => {
+            destroy(coin);
+            scoreLabel.value++;
+            scoreLabel.text = scoreLabel.value;
+        });
+        dino.collides('water', () => {
+            wait(0.05, () => {
+                isJumping = true;
+                dino.jump(currentJumpForce*1.5);
+                // dino.play("hurt");  
+            });
+        })
         // dino.on('headbump', (object) => {
         //     if(object.is('coin-surprise')) {
         //         gameLevel.spawn('$', object.gridPos.sub(0,1));
@@ -385,11 +459,11 @@ function startGame() {
         //     dino.biggify(6);
         // });
 
-        // dino.action(() => {
-        //     if(dino.grounded()) {
-        //         isJumping = false;
-        //     }
-        // });
+        dino.action(() => {
+            if(dino.grounded()) {
+                isJumping = false;
+            }
+        });
         dino.action(() => {
             camPos(dino.pos);
             if(dino.pos.y >= fallDeath){
