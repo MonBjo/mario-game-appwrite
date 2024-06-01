3a35591ad88552693e2f7bf4ca85111f9c817d65
Monica Björk <m.bjork95@gmail.com>
2024-06-01 16:54:11 +0200

    uppdated express

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.use(express.static('.'));
 
-// app.get('/', function (req,res) {
-//     res.send('hellu');
-// });
+app.get('/', function (req,res) {
+    res.send('hellu');
+});
 
 const server = app.listen(8000, () => {
     const port = server.address().port;
-    console.log(`App started at https://localhost:${port}`);
+    console.log(`App started at http://localhost:${port}`);
 });
